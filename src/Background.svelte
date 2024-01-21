2118b338d603c99ba581620fb44e8ec211a08413
Dibyajyoti Dey <dibyajyoti49@outlook.com>
2024-01-21 14:27:53 +0530

    init

---
+++ src/Background.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+    import Parallax from 'parallax-js'
+    import {onMount} from "svelte";
+    import Snow from "./Snow.svelte";
+
+    onMount(()=>{
+        let scene = document.getElementById('scene');
+        let parallaxInstance = new Parallax(scene,{
+            relativeInput: true,
+            clipRelativeInput: true,
+        });
+    })
+</script>
+
+<div id="scene">
+    <img data-depth="0" src="/Asset%2012-01.svg">
+    <img data-depth-y="0.001" src="/Asset%2011-01.svg">
+    <img data-depth-y="0.003" src="/Asset%2010-01.svg">
+    <img data-depth-y="0.006" src="/Asset%209-01.svg">
+    <img data-depth-y="0.008" data-depth-x="0.05" src="/Asset%208-01.svg">
+    <img data-depth-y="0" src="/Asset%207-01.svg">
+    <img data-depth-y="0" data-depth-x="0.02" src="/Asset%206-01.svg">
+    <img data-depth-y="0" data-depth-x="0.03" src="/Asset%205-01.svg">
+    <img data-depth-y="0.08" data-depth-x="0.02" src="/Asset%204-01.svg">
+    <img data-depth-y="0.1" data-depth-x="0.03" src="/Asset%203-01.svg">
+    <img data-depth-y="0.15" data-depth-x="0.05" src="/Asset%202-01.svg">
+    <Snow/>
+    <img class="front" data-depth-y="0.2" data-depth-x="0.15" src="Asset-1-01-01.svg">
+</div>
+
+<div class="overlay"></div>
+
+<style>
+    #scene{
+        position: relative;
+        height: 100vh;
+        width: 100%;
+        left: -2%;
+
+    }
+    .overlay{
+        width: 100%;
+        height: 100vh;
+        background-color: rgba(0,0,0,0.25);
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+    .front{
+        top: 10%;
+        width: 105%;
+        height: 105vh;
+        position: absolute;
+        object-fit: cover;
+    }
+    img{
+        width: 105%;
+        height: 105vh;
+        position: absolute;
+        object-fit: cover;
+
+    }
+</style>
---
+++ src/Hud.svelte
@@ -0,0 +1,109 @@
+<script>
+
+</script>
+<svelte:head>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+    </style>
+</svelte:head>
+<div class="trapezium">
+    <a href="https://twitter.com/itskrak8" class="twitter" target="_blank" rel="noopener noreferrer"><i
+            class="fab fa-twitter"></i><span class="text">Twitter</span></a>
+    <a href="https://github.com/Krak8" class="github" target="_blank" rel="noopener noreferrer"><i
+            class="fab fa-github"></i><span class="text">GitHub</span></a>
+    <a href="https://www.youtube.com/@Krak8" class="youtube" target="_blank" rel="noopener noreferrer"><i
+            class="fab fa-youtube"></i><span class="text">YouTube</span></a>
+    <a href="https://www.instagram.com/imkrak8" class="instagram" target="_blank" rel="noopener noreferrer"><i
+            class="fab fa-instagram"></i><span class="text">Instagram</span></a>
+    <a href="https://www.twitch.tv/imkrak8" class="twitch" target="_blank" rel="noopener noreferrer"><i
+            class="fab fa-twitch"></i><span class="text">Twitch</span></a>
+    <a href="https://discord.com/invite/ZGCmmgR" class="discord" target="_blank" rel="noopener noreferrer"><i
+            class="fab fa-discord"></i><span class="text">Discord</span></a>
+</div>
+
+
+<style>
+
+    @media only screen and (max-width: 500px) {
+
+        .trapezium {
+            width: 100%;
+
+        }
+    }
+
+    @media only screen and (min-width: 500px) {
+
+        .trapezium {
+            width: 60%;
+
+        }
+    }
+
+    .trapezium {
+
+        /*height: 10%;*/
+        min-height: 50px;
+        height: fit-content;
+        background-image: linear-gradient(rgba(0, 0, 0, 0.60), rgba(72, 72, 72, 0.28));
+        clip-path: polygon(0 0, 100% 0, 90% 100%, 10% 100%);
+        display: flex;
+        justify-content: center;
+        gap: 2vh;
+        padding: 3vh 6vh;
+        flex-wrap: wrap;
+    }
+
+    .fab {
+        font-size: 3.2vh;
+    }
+
+    .text {
+        margin-left: 10px;
+        font-family: 'Poppins', sans-serif;
+        font-weight: 600;
+        font-size: 1.7vh;
+
+    }
+    a {
+        display: flex;
+        align-items: center;
+        /*color: #FBF6BC;*/
+        color: white;
+        background-color: rgba(251, 246, 188, 0);
+        /*backdrop-filter: blur(9px);*/
+        border-radius: 9px;
+        /*border-color: #FBF6BC;*/
+        border-width: 3px;
+        border-style: solid;
+        text-decoration: none;
+        transition: background-color 0.5s ease,color 0.5s ease;
+        height: fit-content;
+        padding: 10px 20px;
+    }
+    a:hover{
+        /*background-color: #FBF6BC;*/
+        background-color: white;
+    }
+    .youtube:hover{
+        color: #FF0000;
+    }
+    .twitter:hover {
+        color: #1D9BF0;
+    }
+    .twitch:hover {
+        color: #9146FF;
+    }
+    .discord:hover {
+        color: #5865F2;
+    }
+    .github:hover {
+        color: #000000;
+    }
+    .instagram:hover {
+        color: #E1306C;
+    }
+
+
+
+</style>
